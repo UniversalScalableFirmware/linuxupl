803c6c58303a944f77df31ccd4dfa818a758154f
Wu, Binson <binson.wu@intel.com>
2022-09-28 09:22:13 +0800

    Upstream LinuxUplPkg to USF Github
    [Feature Description]
    LinuxUpl refactoring - upstream LinuxUplPkg to USF Github
    
    1. Add Intel config for Linuxboot building.
    2. Add Osf Bzimage for Meta OS.
    3. Modify LinuxUplBuild.sh to build Intel Bzimage before building
    LinuxUplPkg.
    4. Add -c flag in LinuxUplBuild.sh to control the version of Bzimage.
    EX: bash LinuxUplBuild.sh. -c [INTEL|OSF]
    
    Package/Module: LinuxUpl/LinuxUplPkg
    
    [Impacted Platform]
    GNR_Coreboot

--- LinuxUplBuild.sh
+++ LinuxUplBuild.sh
@@ -4,20 +4,18 @@ BuildTarget="DEBUG"
 BzImagePath=""
 InitramfsPath=""
 WithoutEdk2="TRUE"
-while getopts "hK:I:b:t:w:" arg; do
+LinuxbootConfig="INTEL"
+while getopts "hc:I:b:t:w" arg; do
   case $arg in
     h)
       echo "usage:"
       echo "  -h: help"
-      echo "  -K: Linux Kernel path"
       echo "  -I: optional, initramfs path"
       echo "  -b: build target, default is DEBUG"
       echo "  -t: tool chain, default is GCC5"
       echo "  -w: WithoutEdk2, default is TRUE"
+      echo "  -c: Linuxboot Config [INTEL|OSF], default is INTEL"
       exit 0
-      ;;
-    K)
-      BzImagePath=$OPTARG
       ;;
     I)
       InitramfsPath=$OPTARG
@@ -28,12 +26,11 @@ while getopts "hK:I:b:t:w:" arg; do
     b)
       BuildTarget=$OPTARG
       ;;
+    c)
+      LinuxbootConfig=$OPTARG
+      ;;
   esac
 done
-
-if [[ $BzImagePath = "" ]]; then
-  exit 1
-fi
 
 export WORKSPACE=$(cd `dirname $0`; pwd)
 if [[ $WithoutEdk2 = "FLASE" ]]; then
@@ -42,6 +39,52 @@ fi
 while [ $# -gt 0 ]; do
   shift
 done
+
+if [[ $LinuxbootConfig = "OSF" ]]; then
+  BzImagePath="$WORKSPACE/Bzimage/linuxboot_bzImage_osf"
+elif [[ $LinuxbootConfig = "INTEL" ]]; then
+  BzImagePath="$WORKSPACE/Bzimage/linuxboot_bzImage_intel"
+  if [ ! -f $BzImagePath ]; then
+    echo "Build linuxboot_bzImage_intel ..."
+    if ! command -v go &> /dev/null
+    then
+      echo "Check go ... Fail"
+      echo "Please install go!"
+      exit
+    fi
+
+    #　Prepare linuxboot kernel source code
+    if [ ! -d "$WORKSPACE/lb_kernel" ]; then
+	  git clone https://github.com/linuxboot/linux lb_kernel
+	fi
+    # Set the PATH for build environment
+    export GOPATH="$WORKSPACE/go"
+    export PATH="$WORKSPACE/go/bin:$PATH"
+    export PATH="/usr/local/go/bin:$PATH"
+    # Download and install u-root
+    GO111MODULE=off go get github.com/u-root/u-root
+    GO111MODULE=off go get github.com/nsf/godit
+    # Go to the u-root source code folder and build
+    cd $WORKSPACE/go/src/github.com/u-root/u-root/
+    u-root -build=bb -uinitcmd=systemboot core cmds/exp/{page,ipmidump,dumpebda} github.com/u-root/u-root/cmds/boot/{systemboot,localboot}
+    cd $WORKSPACE
+    # Copy u-root output to linuxboot source folder
+    cp -f /tmp/initramfs.linux_amd64.cpio ./lb_kernel/
+    # Copy coreboot config to linuxboot source
+    cp -f ./Configs/x86_64_defconfig_intel ./lb_kernel/arch/x86/configs/x86_64_defconfig
+    # Build
+    cd ./lb_kernel
+    make mrproper
+    make defconfig
+    make
+    # Copy linuxboot output to coreboot payload folder and rename it to "linuxboot_bzImage_intel"
+    cd $WORKSPACE
+    cp -f ./lb_kernel/arch/x86/boot/bzImage ./Bzimage/linuxboot_bzImage_intel
+  fi
+else
+  echo "Please set Linuxboot Config [INTEL|OSF]"
+  exit 1
+fi
 
 if [[ $WithoutEdk2 = "FALSE" ]]; then
   cd $WORKSPACE/../Edk2
