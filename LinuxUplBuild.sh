fb9224056a93ff05ad89f4cd912d0efbd0f53c40
Wu, Binson <binson.wu@intel.com>
2022-09-28 16:33:57 +0800

    Upstream LinuxUplPkg to USF Github
    [Feature Description]
    LinuxUpl refactoring - upstream LinuxUplPkg to USF Github
    1. Build Bzimage before LinuxUplPkg build if you do not set
    BzImagePath by -K.
    Bzimage uses Linuxboot and U-root.
    2. Add -c to set Linuxboot compiler config file.
    3. Add -l to set Linuxboot commit version.
    4. Add -u to set U-root commit version.
    You can use "bash LinuxUplBuild.sh" to build directly.
    It would use the default setting to build Bzimage.
    You can use -h to check usage.
    
    Hsd-es-id: 15012023220

--- LinuxUplBuild.sh
+++ LinuxUplBuild.sh
@@ -4,20 +4,29 @@ BuildTarget="DEBUG"
 BzImagePath=""
 InitramfsPath=""
 WithoutEdk2="TRUE"
-while getopts "hK:I:b:t:w:" arg; do
+LinuxbootConfig=""
+UrootCommit="c45c826bfd6636aef82077dfbcb0ac96235fb46b"
+LinuxbootCommit="8fe31e0995f048d16b378b90926793a0aa4af1e5"
+while getopts "hK:c:I:b:t:w:l:u:" arg; do
   case $arg in
     h)
       echo "usage:"
       echo "  -h: help"
-      echo "  -K: Linux Kernel path"
+      echo "  -K: Linux Kernel path, default is linuxboot_bzImage_intel"
       echo "  -I: optional, initramfs path"
       echo "  -b: build target, default is DEBUG"
       echo "  -t: tool chain, default is GCC5"
       echo "  -w: WithoutEdk2, default is TRUE"
+      echo "  -c: Linuxboot build config, default is x86_64_defconfig_intel"
+      echo "  -l: Set Linuxboot commit version, default is 8fe31e0995f048d16b378b90926793a0aa4af1e5"
+      echo "  -u: Set Uroot commit version, default is c45c826bfd6636aef82077dfbcb0ac96235fb46b"
       exit 0
       ;;
     K)
       BzImagePath=$OPTARG
+      ;;
+    c)
+      LinuxbootConfig=$OPTARG
       ;;
     I)
       InitramfsPath=$OPTARG
@@ -28,12 +37,14 @@ while getopts "hK:I:b:t:w:" arg; do
     b)
       BuildTarget=$OPTARG
       ;;
+    l)
+      LinuxbootCommit=$OPTARG
+      ;;
+    u)
+      UrootCommit=$OPTARG
+      ;;
   esac
 done
-
-if [[ $BzImagePath = "" ]]; then
-  exit 1
-fi
 
 export WORKSPACE=$(cd `dirname $0`; pwd)
 if [[ $WithoutEdk2 = "FLASE" ]]; then
@@ -43,6 +54,65 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+if [[ $BzImagePath = "" ]]; then
+  mkdir ImgBuild
+  # Set Linuxboot self build bzImage path and config path
+  BzImagePath="$WORKSPACE/ImgBuild/linuxboot_bzImage_intel"
+  if [[ $LinuxbootConfig = "" ]]; then
+    LinuxbootConfig="$WORKSPACE/Configs/x86_64_defconfig_intel"
+  fi
+
+  if [ ! -f $BzImagePath ]; then
+    echo "Build linuxboot_bzImage_intel ..."
+    if ! command -v go &> /dev/null
+    then
+      echo "Check go ... Fail"
+      echo "Please install go!"
+      exit 1
+    fi
+
+    if [ ! -f $LinuxbootConfig ]; then
+      echo "Linuxboot Config not found!"
+      exit 1
+    fi
+
+    #　Prepare linuxboot kernel source code
+    if [ ! -d "$WORKSPACE/ImgBuild/lb_kernel" ]; then
+	    git clone https://github.com/linuxboot/linux ./ImgBuild/lb_kernel
+	  fi
+    # Set the PATH for build environment
+    export GOPATH="$WORKSPACE/ImgBuild/go"
+    export PATH="$WORKSPACE/ImgBuild/go/bin:$PATH"
+    export PATH="/usr/local/go/bin:$PATH"
+    # Download and install u-root
+    GO111MODULE=off go get github.com/u-root/u-root
+    GO111MODULE=off go get github.com/nsf/godit
+    # Go to the u-root source code folder and build
+    cd $WORKSPACE/ImgBuild/go/src/github.com/u-root/u-root/
+    ## Use a steady version or customer
+    git checkout $UrootCommit
+    git rev-parse HEAD
+    u-root -build=bb -uinitcmd=systemboot core cmds/exp/{page,ipmidump,dumpebda} github.com/u-root/u-root/cmds/boot/{systemboot,localboot}
+    cd $WORKSPACE
+    # Build
+    cd ./ImgBuild/lb_kernel
+    ## Use a steady version or customer
+    git checkout $LinuxbootCommit
+    git rev-parse HEAD
+    ## Copy u-root output to linuxboot source folder
+    cp -f /tmp/initramfs.linux_amd64.cpio ./
+    ## Copy coreboot config to linuxboot source
+    cp -f $LinuxbootConfig ./arch/x86/configs/x86_64_defconfig
+    make mrproper
+    make defconfig
+    make
+    # Copy linuxboot output to coreboot payload folder and rename it to "linuxboot_bzImage_intel"
+    cd $WORKSPACE
+    cp -f ./ImgBuild/lb_kernel/arch/x86/boot/bzImage ./ImgBuild/linuxboot_bzImage_intel
+  fi
+fi
+
+echo "Build LinuxUplPkg ..."
 if [[ $WithoutEdk2 = "FALSE" ]]; then
   cd $WORKSPACE/../Edk2
   source ./edksetup.sh
